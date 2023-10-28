b729b9aa632b823f672103d3f650f098c9279028
harsh123-star <68588292+harsh123-star@users.noreply.github.com>
2023-10-28 11:47:11 +0000

    done

--- .devcontainer/01_basics/text.js
+++ .devcontainer/01_basics/text.js
@@ -1,1 +1,13 @@
-console.log("Harsh");
+//console.log("Harsh");
+const accountId=85421;
+var accountName="Harsh";
+let accountPass="587Ha";
+//console.table([accountId , accountName , accountPass])
+accountName="Shivam";
+accountPass="741ha";
+//console.table([accountId , accountName , accountPass]);
+
+let toCheckIt=1;
+let krneTohDe=String(toCheckIt);
+console.log(krneTohDe);
+console.log(typeof krneTohDe);
